b1da8bee65f705a8e0580fff6d5f3cf974cc3a7e
Monika_Bhasin <monika.bhasin@myparkplus.com>
2021-01-24 17:00:38 +0530

    first commit

---
+++ app/src/main/java/com/krsna/mirrordoor/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.krsna.mirrordoor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
