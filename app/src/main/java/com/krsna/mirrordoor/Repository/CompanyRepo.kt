cee543f3bb5f5b655d7c8b4138d0ae2efc1ce714
Monika_Bhasin <monika.bhasin@myparkplus.com>
2021-02-06 21:25:05 +0530

    Handling complex queries in Firestore(consider OR not consider cases). You will never find this thing on Internet..

--- app/src/main/java/com/krsna/mirrordoor/Repository/CompanyRepo.kt
+++ app/src/main/java/com/krsna/mirrordoor/Repository/CompanyRepo.kt
@@ -1,10 +1,17 @@
 package com.krsna.mirrordoor.Repository
 
 import com.example.base.models.BaseResponse
+import com.example.base.utils.AppLogger
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.krsna.mirrordoor.Interfaces.Callbacks
 import com.krsna.mirrordoor.Model.Company
 import com.krsna.mirrordoor.Model.ShowCompanyPayload
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.DisposableHandle
+import kotlinx.coroutines.launch
 
 class CompanyRepo(private val fireStoreDatabase: FirebaseFirestore) {
 
@@ -23,13 +30,24 @@ class CompanyRepo(private val fireStoreDatabase: FirebaseFirestore) {
         }
     }
 
+    private var collectionReference : CollectionReference? = null
+    private var query : Query ? = null
+    private var isFirst = true
     fun showCompanies(showCompanyPayload: ShowCompanyPayload, callback: Callbacks.ShowCompanyCallback) {
-        val companyList : ArrayList<Company> = arrayListOf()
-        fireStoreDatabase.collection("Company")
-            .whereEqualTo("reviews", showCompanyPayload.getReview())
-            .get()
-            .addOnSuccessListener { response ->
-                for(item in response) {
+        val companyList: ArrayList<Company> = arrayListOf()
+        query = null
+        collectionReference = null
+        collectionReference = fireStoreDatabase.collection("Company")
+        isFirst = true
+        CoroutineScope(Dispatchers.IO).launch {
+            addReviewFilter(showCompanyPayload.getReview())
+            addTypeFilter("private")
+
+            val callReference = if(isFirst) {
+                getCollectionReference()
+            } else getQueryReference()
+            callReference?.get()?.addOnSuccessListener { response ->
+                for (item in response) {
                     val company = Company().setCompany(item.getString("company_name"))
                         .setWebsite(item.getString("website"))
                         .setType(item.getString("type"))
@@ -37,9 +55,36 @@ class CompanyRepo(private val fireStoreDatabase: FirebaseFirestore) {
                     companyList.add(company)
                 }
                 callback.showCompanySuccess(companyList)
-            }
-            .addOnFailureListener { exception ->
+            }?.addOnFailureListener { exception ->
                 callback.showCompanyFailure(exception.toString())
             }
+        }
+    }
+    private fun addReviewFilter(review: Int? ) {
+        if (isFirst) {
+            if (review != -1) {
+                query = collectionReference?.whereEqualTo("reviews", 3)!!
+                isFirst = false
+            }
+        } else query?.whereEqualTo("reviews", 3)
+
+    }
+
+    private fun addTypeFilter(type: String?) {
+        if (isFirst) {
+            if (type != null) {
+                query = collectionReference?.whereEqualTo("type", type)!!
+                isFirst = false
+            }
+        } else query?.whereEqualTo("type", type)
+
+    }
+
+    private fun getCollectionReference() : CollectionReference? {
+        return collectionReference
+    }
+
+    private fun getQueryReference() : Query? {
+        return query
     }
 }
